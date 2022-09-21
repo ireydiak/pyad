0138c7ade4c12781da16b0e2166261cc09d57076
Jean-Charles Verdier <jean-charles.verdier@usherbrooke.ca>
2022-09-21 14:57:10 -0400

    added a tex utility script for the drawback2022 paper.

--- scripts/tex-utilities/atk2tex.py
+++ scripts/tex-utilities/atk2tex.py
@@ -26,6 +26,9 @@ def parse_args() -> Namespace:
         "--columns", type=List[str], help="dataframe columns to display", default=None
     )
     parser.add_argument(
+        "--models", type=List[str], help="models to include (defaults to every model)", default=None
+    )
+    parser.add_argument(
         "--table_caption", type=str, default="table caption"
     )
     parser.add_argument(
@@ -37,7 +40,7 @@ def parse_args() -> Namespace:
 def create_template(
         path_to_template: str,
         datasets: List[str],
-        metrics: List[str],
+        headers: List[str],
         structure: dict,
         caption: str,
         label: str,
@@ -59,13 +62,30 @@ def create_template(
     template = latex_jinja_env.get_template(path_to_template)
     buf = template.render(
         datasets=datasets,
-        metrics=metrics,
+        headers=headers,
         structure=structure,
         caption=caption,
         label=label,
         column_positions=column_positions
     )
     return buf
+
+
+def initialize_structure(attack_types: List[str], datasets: List[str]) -> dict:
+    """
+
+    Returns
+    -------
+    attack_type:
+        dataset:
+            [model_1, ..., model_n]
+    """
+    structure = {
+        attack_type: {
+            dataset: [] for dataset in datasets
+        } for attack_type in attack_types
+    }
+    return structure
 
 
 def csv2tex(
@@ -75,44 +95,55 @@ def csv2tex(
         from_folders: List[str],
         cols: List[str],
         table_caption: str,
-        table_label: str
+        table_label: str,
+        models_to_include: List[str] = None
 ) -> None:
+    # setup
+    to_process, datasets = [], []
+    headers = ["Traffic type", "Datasets"]
+    datasets = [p.split(os.path.sep)[-1] for p in from_folders]
+    structure, models = {}, set()
     # files to process
-    to_process = []
-    datasets = []
     for base_root in from_folders:
         assert os.path.exists(base_root), f"path {base_root} does not exist, aborting"
         datasets.append(base_root.split(os.path.sep)[-1])
-        to_process.extend(list(
-            map(lambda folder: os.path.join(base_root, folder, fname), os.listdir(base_root))
-        ))
-    metrics, structure = cols, {}
-    # will be used a table titles
-    datasets = [p.split(os.path.sep)[-1] for p in from_folders]
-    structure = {}
+        for model_name in os.listdir(base_root):
+            if model_name in set(models_to_include):
+                to_process.append(
+                    os.path.join(base_root, model_name, fname)
+                )
+    # contains the data that will be displayed
+    structure = initialize_structure(cols, datasets)
     # read csv files
     for path_to_file in to_process:
+        # get name of model and dataset
+        model_name = path_to_file.split(os.path.sep)[-2]
+        dataset_name = path_to_file.split(os.path.sep)[-3]
         if os.path.exists(path_to_file):
             # load dataframe
             df = pd.read_csv(path_to_file)
             # read last line
+            cols = sorted(list(set(df.columns) & set(cols)))
             data = df.loc[len(df) - 1, cols]
-            model_name = path_to_file.split(os.path.sep)[-2]
-            # initialize structure for new models
-            if not structure.get(model_name, None):
-                structure[model_name] = []
+            models.add(model_name)
             # add average and standard deviation to structure
-            values = {}
-            for k, v in data.items():
-                avg = v.split("(")[0].strip()
-                std = re.search(r'\((.*?)\)', v).group(1)
-                values[k] = {"avg": avg, "std": std}
-            structure[model_name].append(values)
+            for attack_name, score in data.items():
+                avg = score.split("(")[0].strip()
+                std = re.search(r'\((.*?)\)', score).group(1)
+                structure[attack_name][dataset_name].append(
+                    {"avg": avg, "std": std}
+                )
+        else:
+            for k in structure.keys():
+                structure[k][dataset_name].append({"avg": 0.0, "std": 0.0})
+    headers.extend(
+        sorted(list(models))
+    )
     buf = create_template(
-        path_to_tmpl, list(datasets), list(metrics), structure,
+        path_to_tmpl, list(datasets), headers, structure,
         caption=table_caption,
         label=table_label,
-        column_positions="l" + "c" * ((len(metrics) * 2 - 1) * len(datasets) + 1)
+        column_positions="l" * (len(headers) + 2)
     )
     with open(out_fname, "w") as f:
         f.write(buf)
@@ -127,7 +158,8 @@ def main():
         from_folders=args.folders,
         cols=args.columns,
         table_caption=args.table_caption,
-        table_label=args.table_label
+        table_label=args.table_label,
+        models_to_include=args.models
     )
 
 
